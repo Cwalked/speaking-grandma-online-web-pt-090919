d53c4b88b4a8f7f7e663ed3514936cc250cd6275
Courtney Walker <cwalked@gmail.com>
2019-09-13 15:05:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,9 +5,10 @@ def speak_to_grandma (string)
   if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
     
-    elsif 
+    elsif string == string.upcase
+    "NO, NOT SINCE 1938!"
     
-else
+else string != string.upcase
   "HUH?! SPEAK UP, SONNY!"
   
 end
